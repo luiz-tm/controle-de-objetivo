41a3f0e187b194fedc274380dfdf44c721895cad
luiz-85 <luizhenriquetm123@gmail.com>
2022-03-11 19:56:42 -0300

    Adicionada última atualização, botão para zerar informações e valor restante.

--- script.js
+++ script.js
@@ -4,18 +4,42 @@ var objetivoInput = document.getElementById('input-objective')
 var saldoInput = document.getElementById('input-saldo')
 var porcentagemObjetivo = document.getElementById('progress-percent')
 var progressBar = document.getElementById('div-bar')
+var lastUpdate = document.getElementById('last-update')
+var restante = document.getElementById('restante-js')
 
 var Storage = localStorage;
 
+/* window.alert(`${dia}/${mes}/${ano} - ${hora}:${minuto}:${segundo}`) */
+
+
 const updateObjective = () => {
+    updateDate()
     localStorage.setItem('objetivo', objetivoInput.value);
     updateApp()
 }
 
 const updateSaldo = () => {
+    updateDate()
     valueSaldo = (Number(saldoAtual.value) + Number(saldoInput.value.replace(',','.')))
     localStorage.setItem('saldo', valueSaldo);
     saldoInput.value = ``
+    updateApp()
+}
+
+const updateDate = () => {
+    var data = new Date()
+    var dia = data.getDate()
+    var mes = data.getMonth()+1
+    var ano = data.getFullYear()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var segundo = data.getSeconds()
+    localStorage.setItem('gettime', `${dia}/${mes}/${ano} - ${hora}:${minuto}:${segundo}`)
+}
+
+const resetInfo = () => {
+    localStorage.setItem('saldo', 0)
+    localStorage.setItem('objetivo', 0)
     updateApp()
 }
 
@@ -26,6 +50,7 @@ const updateApp = () => {
 
     var valueObjetivo = Number(objetivoInput.value).toFixed(2)
     var valueSaldo = Number(saldoAtual.value).toFixed(2)
+    var valueRestante = Number(valueObjetivo-valueSaldo).toFixed(2)
 
     porcentagemObjetivo.value = (valueSaldo * 100) / valueObjetivo
 
@@ -36,8 +61,19 @@ const updateApp = () => {
     
     objetivoAtual.innerHTML = `R$ ${valueObjetivo}`
     saldoAtual.innerHTML = `R$ ${valueSaldo}`
+
+    if(valueObjetivo-valueSaldo >= 0) { restante.innerHTML = `R$ ${valueRestante}`} else 
+    { restante.innerHTML = `Ultrapassado`}
+
     
     porcentagemObjetivo.innerHTML = `${porcentagemObjetivo.value.toFixed(2)}%`
+
+    lastUpdate.innerHTML = `Última atualização: <strong>${localStorage.getItem('gettime')}</strong>`
+
+    if(localStorage.getItem('gettime') == null)
+    {
+        lastUpdate.innerHTML = `Última atualização: <strong>Nunca</strong>`
+    }
 
     if(porcentagemObjetivo.value >= 0)
     {
